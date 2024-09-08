9d0f35d3559163f202dcb516037b074251a2eb90
Nandini Pandey <pandeynandini2004@gmail.com>
2024-09-08 21:07:52 +0530

    modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Leaderboard.js 	new file:   src/components/MCQ.css 	new file:   src/components/MCQ.js 	new file:   src/components/Progress.css 	new file:   src/components/Progress.js 	new file:   src/components/Results.js 	new file:   src/components/Story.css 	new file:   src/components/Story.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

---
+++ src/components/MCQ.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MCQ = ({ question, options, onAnswer }) => {
+  return (
+    <div className="mcq-card">
+      <h3>{question}</h3>
+      <div className="options">
+        {options.map((option, index) => (
+          <button key={index} onClick={() => onAnswer(option)}>
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MCQ;
---
+++ src/components/Progress.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Progress.css'; 
+
+
+const Progress = ({ currentQuestion, totalQuestions }) => {
+  return (
+    <div className="progress-indicator">
+      <p>Question {currentQuestion} of {totalQuestions}</p>
+    </div>
+  );
+};
+
+export default Progress;
